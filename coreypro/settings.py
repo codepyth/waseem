c5c80b8d0c4738ca3caa977ad068012ef7f38765
codepyth <wasimbaloch9@gmail.com>
2021-08-09 16:32:58 +0500

    heroku settings

--- coreypro/settings.py
+++ coreypro/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
+import django_heroku
+import dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -51,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'coreypro.urls'
@@ -135,6 +139,9 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "media"),
 )
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
@@ -153,3 +160,5 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'waseempy@gmail.com'
 EMAIL_HOST_PASSWORD = 'clrijkmuyzesangu'
+
+django_heroku.settings(locals())

c69d6f666b8a822851dce396a6f7413d738c0459
codepyth <wasimbaloch9@gmail.com>
2021-08-10 12:55:03 +0500

    removed  forgot password credentials

--- coreypro/settings.py
+++ coreypro/settings.py
@@ -158,7 +158,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'waseempy@gmail.com'
-EMAIL_HOST_PASSWORD = 'clrijkmuyzesangu'
+EMAIL_HOST_USER = 'YOUR EMAIL'
+EMAIL_HOST_PASSWORD = 'EMAIL PASSWORD'
 
 django_heroku.settings(locals())
